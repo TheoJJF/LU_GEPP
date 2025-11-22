18966bce095a591ce65d97007b92e312b4e521c4
TheoJJF <itstheo215@gmail.com>
2025-11-22 02:28:47 -0800

    Add test case and data for test

---
+++ tests/SparseValidation.m
@@ -0,0 +1,18 @@
+classdef SparseValidation < matlab.unittest.TestCase
+
+    methods (Test)
+        
+        function case1(testCase)
+            data = load(fullfile(pwd,"../data","SparseValidation.mat"));
+            verification = load(fullfile(pwd,"../data","expected.mat"));
+
+            A = data.A;
+            b = data.b;
+            expected = verification.expected;
+
+            actual = SGEPP(A,b);
+            testCase.verifyEqual(actual,expected);
+        end
+    end
+
+end
