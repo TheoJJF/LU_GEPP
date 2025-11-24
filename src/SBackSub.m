fac08856889374b76eb2780568d72cdd9408f582
TheoJJF <itstheo215@gmail.com>
2025-11-23 19:39:24 -0800

    Makeshift test passed

--- src/SBackSub.m
+++ src/SBackSub.m
@@ -12,10 +12,13 @@ function x = SBackSub(U,d)
     for i = n-1:-1:1
         pivot_recip = 1/U{i}(i);
 
-        for j = i+1:n
-            
+        for j = 1:numEntries(U{i})
+            all_keys = keys(U{i});
+            jth_key = all_keys(j);
+
+            x(i) = x(i)+U{i}(jth_key)*x(jth_key);
         end
         
-
+        x(i) = (d(i)-x(i))*pivot_recip;
     end
 end
