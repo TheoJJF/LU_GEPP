2501cf517ebe5b5c02cf786de61b7f02cc1d7f8d
Theo Fung <131817438+TheoJJF@users.noreply.github.com>
2026-03-19 21:41:06 -0700

    Remove comment
    Removed comment about Gauss Elimination with Partial Pivoting.

--- src/matlab/LU.m
+++ src/matlab/LU.m
@@ -24,7 +24,6 @@ Returns: L (Matrix), U (Matrix), P (Matrix) d (Vector)
     P = eye(size(A));
     L = zeros(size(A));
     
-    % Perform Gauss Elimination with Partial Pivoting
     for i = 1:n-1
         [Aii_max,i_max_subcol] = max(abs(A(i:n,i)));
         
